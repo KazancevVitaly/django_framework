50c0f1049c4a08a24210ed739a49781bce4d1d0e
vitaly <kazancevvitalijj@gmail.com>
2022-06-02 20:42:52 +0800

    mainapp.context_processors

--- BraniacLMS/config/settings.py
+++ BraniacLMS/config/settings.py
@@ -69,7 +69,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 "django.template.context_processors.media",
-                "mainapp.context_processors.example.simple_context_processor",
+                # "mainapp.context_processors.example.simple_context_processor",
             ],
         },
     },
