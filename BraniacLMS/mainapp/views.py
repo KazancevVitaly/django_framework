63d117f118e5bfdb2c31047f8b88924cea00d13c
vitaly <kazancevvitalijj@gmail.com>
2022-06-02 20:00:34 +0800

    profile_edit

--- BraniacLMS/mainapp/views.py
+++ BraniacLMS/mainapp/views.py
@@ -55,5 +55,5 @@ class DocSitePageView(TemplateView):
     template_name = "mainapp/doc_site.html"
 
 
-class LoginPageView(TemplateView):
-    template_name = "mainapp/login.html"
+# class LoginPageView(TemplateView):
+#     template_name = "mainapp/login.html"
