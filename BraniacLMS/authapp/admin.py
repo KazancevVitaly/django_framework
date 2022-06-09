eb8d1e76f49fdd93f7e0553f0160301fc645788e
KazancevVitaly <70364854+KazancevVitaly@users.noreply.github.com>
2022-06-09 18:05:34 +0800

    Geek Brains. Django Framework. ДЗ Урока6. Казанцев Виталий. (#6)
    Административный раздел Django и работа с формами. Lesson6.

---
+++ BraniacLMS/authapp/admin.py
@@ -0,0 +1,8 @@
+from authapp import models
+from django.contrib import admin
+
+
+@admin.register(models.CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ["id", "username", "email", "is_active", "date_joined"]
+    ordering = ["-date_joined"]
