12198f08e8f024c07d6cde02233db10c7d3cba5d
vitaly <kazancevvitalijj@gmail.com>
2022-06-13 17:49:44 +0800

    internationalisation

--- BraniacLMS/config/settings.py
+++ BraniacLMS/config/settings.py
@@ -65,6 +65,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
@@ -236,3 +237,5 @@ CACHES = {
         },
     }
 }
+
+LOCALE_PATHS = [BASE_DIR / "locale"]
